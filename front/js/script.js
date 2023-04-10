53c42528d5f164d817084be1c28e6e30bd9a9c1d
Fabien Felix <ffabien.ga1@gmail.com>
2023-04-10 18:48:55 +0200

    Update Js

--- front/js/script.js
+++ front/js/script.js
@@ -1,18 +1,65 @@
 fetch("http://localhost:3000/api/products")
     .then(res => res.json())
-    .then(data => addProducts(data))
+    .then(data => {
+        console.log(data)
+        return addProducts(data)
+    })
 
-    function addProducts(donnees) {
-        
-        const imageUrl = donnees[0].imageUrl
+    // altTxt: "Photo d'un canapé bleu, deux places"
+    // colors: (3) ['Blue', 'White', 'Black']
+    // description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    // imageUrl: "http://localhost:3000/images/kanap01.jpeg"
+    // name: "Kanap Sinopé"
+    // price: 1849
+    // _id: "107fb5b75607497b96722bda5b504926"
 
-        const anchor = document.createElement("a")
-        anchor.href = imageUrl
-        anchor.text = "Un canapé"
+function addProducts(donnees) {
+    const id = donnees[0]._id
+    const imageUrl = donnees[0].imageUrl
+    const altTxt = donnees[0].altTxt
+    const image = makeImage(imageUrl, altTxt)
 
-        const items = document.querySelector("#items")
-        items.appendChild(anchor)
-    }
+    const anchor = makeAnchor(id)
+    const article = makeArticle()
+    article.appendChild(image)
+    appendChildren(anchor, article)
+}
 
+function makeAnchor(id) {
+    const anchor = document.createElement("a")
+    anchor.href = "./product.html?id=" + id
+    return anchor
+}
+
+function appendChildren(anchor, article) {
+    const items = document.querySelector("#items")
+    items.appendChild(anchor)
+    anchor.appendChild(article)
+}
+
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    return image
+}
+
+function makeArticle() {
+    const article = document.createElement("article")
+    const h3 = makeH3()
+    const p = makeParagraph()
+    // article.appendChild(image)
+    // article.appendChild(h3)
+    // article.appendChild(p)
+    console.log(article)
+    return article
+}
+
+function makeH3() {
+
+}
+function makeParagraph() {
+
+}
 
 
