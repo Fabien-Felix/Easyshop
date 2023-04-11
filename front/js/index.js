0a40f6ca4ca065e9d663db56bbea289ee4b3cadd
Fabien Felix <ffabien.ga1@gmail.com>
2023-04-11 10:37:51 +0200

    Update js

---
+++ front/js/index.js
@@ -0,0 +1,94 @@
+fetch("http://localhost:3000/api/products")
+    .then(res => res.json())
+    
+    // .then(data => {
+
+    .then(function(products){
+        // console.log(data)
+        // return addProducts(data)
+
+        for (let i = 0; i < products.length; i++){
+        document.querySelector(".items").innerHTML += 
+                `<a href="./product.html?id=${products[i]._id}">
+                    <article>
+                        <img src=${products[i].imageUrl} alt=${products[i].altTxt}>
+                        <h3 class="productName">${products[i].name}</h3>
+                        <p class="productDescription">${products[i].description}</p>
+                        <p class="productColor">${products[i].colors}</p>
+                        <p class="productPrice">${products[i].price} €</p>
+                        </article>
+                        </a>`;
+                    }})
+    
+                    
+    .catch(function(error){
+        console.log('erreur est survenue')
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addProducts(donnees) {
+//     const id = donnees[0]._id
+//     const imageUrl = donnees[0].imageUrl
+//     const altTxt = donnees[0].altTxt
+//     const image = makeImage(imageUrl, altTxt)
+
+//     const anchor = makeAnchor(id)
+//     const article = makeArticle()
+//     article.appendChild(image)
+//     appendChildren(anchor, article)
+// }
+
+// function makeAnchor(id) {
+//     const anchor = document.createElement("a")
+//     anchor.href = "./product.html?id=" + id
+//     return anchor
+// }
+
+// function appendChildren(anchor, article) {
+//     const items = document.querySelector("#items")
+//     items.appendChild(anchor)
+//     anchor.appendChild(article)
+// }
+
+// function makeImage(imageUrl, altTxt) {
+//     const image = document.createElement("img")
+//     image.src = imageUrl
+//     image.alt = altTxt
+//     return image
+// }
+
+// function makeArticle() {
+//     const article = document.createElement("article")
+//     const h3 = makeH3()
+//     const p = makeParagraph()
+//     // article.appendChild(image)
+//     // article.appendChild(h3)
+//     // article.appendChild(p)
+//     console.log(article)
+//     return article
+// }
+
+// function makeH3() {
+
+// }
+// function makeParagraph() {
+
+// }
+
+
